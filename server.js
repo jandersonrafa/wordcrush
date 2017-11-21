15620ebad71ab3b7672fbcbac135d79092c45a41
janderson.rosa <janderson_raffael@hotmail.com>
2017-11-21 00:18:16 -0200

    prod

--- server.js
+++ server.js
@@ -16,57 +16,59 @@
 //     ? console.error(error)
 //     : console.info(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
 // ));
-    // var express = require('express');
-    // var app = express();
-    // var httpProxy = require('http-proxy');
-    // var proxy = httpProxy.createProxyServer({});
-    // const PORT = process.env.PORT || 8080;
-    // const path = require('path');
-    
-    // app.use(express.static(path.join(__dirname, 'dist')));
-    // // app.get('/', function(request, response) {
-    // // response.sendFile(__dirname + '/dist/index.html');
-    // // });
-    // app.get('/api/*', function(req, res) {
-    //     "use strict";
-    //     return proxy.proxyRequest(req, res, {
-    //         host: "localhost",
-    //         port: 5000
-    //     });
-    // });
-    
-    // app.listen(PORT);
+// var express = require('express');
+// var app = express();
+// var httpProxy = require('http-proxy');
+// var proxy = httpProxy.createProxyServer({});
+// const PORT = process.env.PORT || 8080;
+// const path = require('path');
 
-    // var express = require('express');
-    // const path = require('path');
+// app.use(express.static(path.join(__dirname, 'dist')));
+// // app.get('/', function(request, response) {
+// // response.sendFile(__dirname + '/dist/index.html');
+// // });
+// app.get('/api/*', function(req, res) {
+//     "use strict";
+//     return proxy.proxyRequest(req, res, {
+//         host: "localhost",
+//         port: 5000
+//     });
+// });
 
-    // var app = express();
-    //     const PORT = process.env.PORT || 8080;
+// app.listen(PORT);
 
-    // app.use(express.static(path.join(__dirname, 'dist')));
-    // // app.use(express.static(__dirname + '/dist/index.html'));
-    // // app.use(app.router);
-    
-    // app.get('*', function(req, res){
-    //     res.redirect(302, 'http://localhost:5000' + req.path);
-    // });
-    
-    // app.listen(PORT);
+// var express = require('express');
+// const path = require('path');
+
+// var app = express();
+//     const PORT = process.env.PORT || 8080;
+
+// app.use(express.static(path.join(__dirname, 'dist')));
+// // app.use(express.static(__dirname + '/dist/index.html'));
+// // app.use(app.router);
+
+// app.get('*', function(req, res){
+//     res.redirect(302, 'http://localhost:5000' + req.path);
+// });
+
+// app.listen(PORT);
 
 
-    var express  = require('express');
-    var app      = express();
-    var httpProxy = require('http-proxy');
-    var apiProxy = httpProxy.createProxyServer();
-    var serverOne = 'https://wordcrush-back.herokuapp.com';
+var express = require('express');
+var app = express();
+var httpProxy = require('http-proxy');
+var apiProxy = httpProxy.createProxyServer();
+var serverOne = 'https://wordcrush-back.herokuapp.com';
+const PORT  = process.env.PORT || 8080;
 
-        const path = require('path');
 
-    app.use(express.static(path.join(__dirname, 'dist')));
+const path = require('path');
 
-    app.all("/api/*", function(req, res) {
-        console.log('redirecting to Server1');
-        apiProxy.web(req, res, {target: serverOne});
-    });
-    
-    app.listen(8080);
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.all("/api/*", function (req, res) {
+    console.log('redirecting to Server1');
+    apiProxy.web(req, res, { target: serverOne });
+});
+
+app.listen(PORT);
