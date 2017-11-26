d52ba1fa03ea8f58eeaa82a9bf0099de7e405157
janderson.rosa <janderson_raffael@hotmail.com>
2017-11-26 14:14:47 -0200

    play game

--- client/pages/playGame/playGameCtrl.js
+++ client/pages/playGame/playGameCtrl.js
@@ -15,19 +15,21 @@ export default class PlayGameController {
   }
 
   checkWords() {
+    var mapInverse = this.settings.mapCharacters[0].map((col, i) => this.settings.mapCharacters.map(row => row[i]));
     this.settings.listRandomKeywordHelp.forEach(kh => {
       var listContains = this.settings.mapCharacters.filter( line => {
         return line.map(k => k.character).join("").toUpperCase().indexOf(kh.dsKeyword.toUpperCase()) > -1
       })
-      debugger
-      if (listContains.length >0) {
-        kh.checked = true;
-      }
+      var listContainsInverse = mapInverse.filter( line => {
+        return line.map(k => k.character).join("").toUpperCase().indexOf(kh.dsKeyword.toUpperCase()) > -1
+      })
+      kh.checked = listContains.length >0 || listContainsInverse.length >0;
     });
   }
 
   select(char, x, y) {
-    if (this.primarySelect) {
+    var blSegundaVez = this.primarySelect != null
+    if (blSegundaVez) {
       var primaryPositionX = this.primarySelect.x
       var primaryPositionY = this.primarySelect.y
       var primaryCharacter = this.primarySelect.character
@@ -45,13 +47,22 @@ export default class PlayGameController {
         this.settings.mapCharacters[primaryPositionX][primaryPositionY].character = aux
 
         this.primarySelect = undefined
+        this.settings.mapCharacters[primaryPositionX][primaryPositionY].selected = false
       }
     } else {
       this.primarySelect = char
       this.primarySelect.x = x
       this.primarySelect.y = y
+      this.primarySelect.selected = true
     }
-    // char.select=true
+    this.checkWords()
   }
 
+  getTotalPalavrasRestantes() {
+    return this.settings.listRandomKeywordHelp.filter(kh => !kh.checked).length
+  }
+
+  getTotalPalavras() {
+    return this.settings.listRandomKeywordHelp.length
+  }
 }
