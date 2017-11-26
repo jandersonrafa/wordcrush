e81694df582305e38cdf35b5c7e39f183bf3a99d
janderson.rosa <janderson_raffael@hotmail.com>
2017-11-26 17:31:29 -0200

    sad

--- client/pages/appBaseSecured/appBaseSecuredCtrl.js
+++ client/pages/appBaseSecured/appBaseSecuredCtrl.js
@@ -2,10 +2,6 @@ import './assets/css/paper-kit.css?v=2.1.0'
 import './assets/js/paper-kit.js?v=2.1.0'
 
 import fogLow from './assets/img/fog-low.png'
-import creativeTimWhiteSlim2 from './assets/img/creative-tim-white-slim2.png'
-import soroushKarimi from './assets/img/soroush-karimi.jpg'
-import federicoBeccari from './assets/img/federico-beccari.jpg'
-import joshuaStannard from './assets/img/joshua-stannard.jpg'
 import wordcrush from './assets/img/wordcrush.jpg'
 import clouds from './assets/img/clouds.png'
 
